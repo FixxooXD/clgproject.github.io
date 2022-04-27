2d2810dca6fcf0d764e03a1b220c0a8d8b7d5a95
Raj palmal <rajpalmal40@gmail.com>
2022-04-27 23:14:56 +0530

    add functionality to the homee calculator

---
+++ home.js
@@ -0,0 +1,17 @@
+const clrBtn = document.getElementById("del");
+let result= document.getElementById("res");
+console.log(result);
+
+
+function number(input){
+     document.getElementById("res").value+=input;
+ } 
+
+ function cal(){
+     var output =  eval(document.getElementById("res").value);
+    document.getElementById("res").value = output;
+ }
+ 
+ function clear(){
+     document.getElementById("res").value = "";
+ }
